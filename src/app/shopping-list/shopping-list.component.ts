08d10087bfedaddcfc3864fedeef31342f2b852a
bhagath <bhagath@getcustomexposure.com>
2018-01-27 19:14:50 +0530

    dispatch action

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
