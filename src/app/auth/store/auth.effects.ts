d27b1c2ce99d6a93840e9a52d35d645cdcad5cb2
bhagath <bhagath@getcustomexposure.com>
2018-01-29 14:49:18 +0530

    Auth signup
     A closer look at Effects Auth Effects and Actions Effects How they work adding Auth Signup

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
+import { Router } from '@angular/router';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
 import { fromPromise } from 'rxjs/observable/fromPromise';
@@ -35,7 +37,43 @@ export class AuthEffects {
 			];
 		});
 
-	constructor(private actions$: Actions) {
+		@Effect() 
+		authSignin = this.actions$
+		.ofType(AuthActions.TRY_SIGNIN)
+		.map((action: AuthActions.TrySignup) => {
+			return action.payload;
+		})
+		.switchMap((authData: {username: string, password: string}) => {
+			return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username,
+			authData.password));
+		})
+		.switchMap(() => {
+			return fromPromise(firebase.auth().currentUser.getIdToken());
+		})
+		.mergeMap((token: string) => {
+			this.router.navigate(['/']);
+			return [
+			{
+				type: AuthActions.SIGNIN
+			},
+
+			{
+				type: AuthActions.SET_TOKEN,
+				payload: token
+
+			}	
+			
+			];
+		});
+
+		@Effect({dispatch: false})
+		authLogout = this.actions$
+			.ofType(AuthActions.LOGOUT)
+			.do(() => {
+					this.router.navigate(['/']);
+			});
+
+		constructor(private actions$: Actions, private router: Router) {
 
 	}
 }
